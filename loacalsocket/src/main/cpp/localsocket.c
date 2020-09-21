aa899ed3613e89191d7c75374670eae3346f1b91
pananfly <panjianan@panjianandeMacBook-Pro.local>
2020-09-21 22:26:12 +0800

    Search libcutils source code.

---
+++ loacalsocket/src/main/cpp/localsocket.c
@@ -0,0 +1,11 @@
+//
+// Created by pananfly on 20/9/21.
+//
+#include <sys/socket.h>
+//#include <cutils/sockets.h>
+
+int initLocalSocket(char* path)
+{
+//    android_get_control_socket(path);
+    return 0;
+}
